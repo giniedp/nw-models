cf04a88d6c2e60cb0a791779e878412371a2af74
Ginie <github@ginie.eu>
2024-06-29 16:04:21 +0200

    fixed convert-slices command

--- packages/cli/commands/convert-slices.ts
+++ packages/cli/commands/convert-slices.ts
@@ -1,10 +1,12 @@
 import 'colors'
 import { program } from 'commander'
+import fs from 'node:fs'
 import path from 'node:path'
-
+import { cpus } from 'os'
 import { CONVERT_DIR, UNPACK_DIR } from '../env'
 import { objectStreamConverter } from '../tools/object-stream-converter'
-import { logger, wrapError } from '../utils'
+import { glob, logger, mkdir, wrapError } from '../utils'
+import { withProgressBar } from '../utils/progress'
 
 program
   .command('convert-slices')
@@ -18,11 +20,33 @@ program
     const inputDir = options.input
     const outputDir = options.output
 
-    await objectStreamConverter({
-      input: path.join(inputDir, 'slices'),
-      output: path.join(outputDir, 'slices'),
-      pretty: true,
-      threads: Math.min(options.threads, 10),
-      stdio: 'inherit',
-    }).catch(wrapError('object-stream-converter failed'))
+    await copyCommonFiles(inputDir, outputDir)
+    await convertObjectStreams({ inputDir, outputDir })
   })
+
+async function copyCommonFiles(inputDir: string, outputDir: string) {
+  const files = await glob(path.join(inputDir, '**', '*.json'))
+  await withProgressBar({ name: 'Copy JSON', tasks: files }, async (file, i, log) => {
+    const relPath = path.relative(inputDir, file)
+    const outPath = path.join(outputDir, relPath)
+    log(relPath)
+    await mkdir(path.dirname(outPath), { recursive: true })
+    await fs.promises.copyFile(file, outPath)
+  })
+}
+
+async function convertObjectStreams({ inputDir, outputDir }: { inputDir: string; outputDir: string }) {
+  await withProgressBar(
+    { name: 'Object Streams', tasks: ['libs', 'coatgen', 'sharedassets', 'slices'] },
+    async (dir, i, log) => {
+      log(dir)
+      await objectStreamConverter({
+        input: path.join(inputDir, dir),
+        output: path.join(outputDir, dir),
+        pretty: true,
+        threads: Math.min(cpus().length, 10),
+        stdio: 'pipe',
+      }).catch(wrapError('object-stream-converter failed'))
+    },
+  )
+}
